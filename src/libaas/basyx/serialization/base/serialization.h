36b842b45fdacff48df5356005e7b042d0246c97
Tobias Klausmann <tobias.klausmann@lenze.com>
2021-10-26 15:53:40 +0200

    Add a private namespace for serialization
    This will be used in the next commits

---
+++ src/libaas/basyx/serialization/base/serialization.h
@@ -0,0 +1,10 @@
+#ifndef SERIALIZATION_H
+#define SERIALIZATION_H
+
+
+// Forward declarations for private classes
+namespace basyx::serialization::priv {
+class Deserializer;
+}
+
+#endif // SERIALIZATION_H
