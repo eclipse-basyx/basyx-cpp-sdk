0fbee81696a933e8d0a2d2c56676ee7512759360
Tobias Klausmann <tobias.klausmann@lenze.com>
2021-11-01 09:58:59 +0100

    Asset: Add AssetKind to asset

--- src/libaas/basyx/asset/asset.h
+++ src/libaas/basyx/asset/asset.h
@@ -4,6 +4,7 @@
 #include <basyx/submodel.h>
 #include <basyx/reference.h>
 #include <basyx/identifiable.h>
+#include <basyx/enums/AssetKind.h>
 
 #include <util/optional/optional.hpp>
 
@@ -14,6 +15,9 @@ class Asset :
 	public Identifiable, 
 	public ModelType<ModelTypes::Asset>
 {
+private:
+   // Legacy AAS Part 1 - V2
+   util::optional<AssetKind> assetKind;
 public:
 	Asset(util::string_view idShort, Identifier identifier) : Identifiable(idShort, std::move(identifier)) {};
 	Asset(util::string_view idShort, util::string_view id) : Identifiable(idShort, id) {};
@@ -23,6 +27,10 @@ public:
 
 	Asset(Asset &&) = default;
 	Asset& operator=(Asset &&) = default;
+
+   // Legacy AAS Part 1 - V2
+   AssetKind getAssetKind() const { return assetKind.value(); };
+   void setAssetKind(AssetKind kind) { this->assetKind = kind; };
 };
 
 };
