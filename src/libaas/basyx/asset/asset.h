7bb8346b80f4ab5ba37f333d26304532228b7eb9
Tobias Klausmann <tobias.klausmann@lenze.com>
2021-11-19 15:52:17 +0100

    asset: Expand to allow (de)-serialization

--- src/libaas/basyx/asset/asset.h
+++ src/libaas/basyx/asset/asset.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <basyx/base/token.h>
+
+#include <basyx/serialization/base/serialization.h>
+
 #include <basyx/base/elementcontainer.h>
 #include <basyx/submodel.h>
 #include <basyx/reference.h>
@@ -11,6 +15,9 @@
 namespace basyx
 {
 
+using namespace basyx::base;
+using namespace basyx::serialization::priv;
+
 class Asset : 
 	public Identifiable, 
 	public ModelType<ModelTypes::Asset>
@@ -18,6 +25,8 @@ class Asset :
 private:
    // Legacy AAS Part 1 - V2
    util::optional<AssetKind> assetKind;
+   util::optional<Reference> billOfMaterialModelRef;
+   util::optional<Reference> assetIdentificationModelRef;
 public:
 	Asset(util::string_view idShort, Identifier identifier) : Identifiable(idShort, std::move(identifier)) {};
 	Asset(util::string_view idShort, util::string_view id) : Identifiable(idShort, id) {};
@@ -29,8 +38,50 @@ public:
 	Asset& operator=(Asset &&) = default;
 
    // Legacy AAS Part 1 - V2
-   AssetKind getAssetKind() const { return assetKind.value(); };
+   const util::optional<AssetKind> & getAssetKind() {
+      return assetKind;
+   };
    void setAssetKind(AssetKind kind) { this->assetKind = kind; };
+
+   util::optional<Reference> & getBillOfMaterialModelRef() {
+      return billOfMaterialModelRef;
+   }
+   void setBillOfMaterialModelRef(Reference bomModelRef) {
+      billOfMaterialModelRef = bomModelRef;
+   }
+
+   const util::optional<Reference> & getAssetIDentificationModelRef() {
+      return assetIdentificationModelRef;
+   }
+   const void setAssetIdentificationModelRef(Reference assetIdentModelRef) {
+      assetIdentificationModelRef = assetIdentModelRef;
+   }
+
+   // Referable - special purpose
+   void setIdShort(Token<Deserializer> t, std::string idShort) {
+      this->Identifiable::setIdShort(idShort);
+   }
+   void setParent(Token<Deserializer> t, Referable * referable) {
+      this->Identifiable::setParent(referable);
+   }
+   void setCategory(Token<Deserializer> t, std::string category) {
+      this->Identifiable::setCategory(category);
+   }
+   void setDisplayname(Token<Deserializer> t, langstringset_t displayname) {
+      this->Identifiable::setDisplayname(displayname);
+   }
+   void setDescription(Token<Deserializer> t, langstringset_t description) {
+      this->Identifiable::setDescription(description);
+   }
+
+   // Identifiable - special purpose
+   void setIdentification(Token<Deserializer> t, Identifier identifier) {
+      this->Identifiable::setIdentification(identifier);
+   }
+   void setAdministration(Token<Deserializer> t,
+                          AdministrativeInformation administrativeInformation) {
+      this->Identifiable::setAdministration(administrativeInformation);
+   }
 };
 
 };
