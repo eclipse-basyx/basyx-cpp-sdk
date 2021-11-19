7adc7462b5d1c063f0e54ba8fb44c653348b7365
Tobias Klausmann <tobias.klausmann@lenze.com>
2021-11-19 15:54:21 +0100

    ElementVector: Allow usage of initalizer lists

--- src/libaas/basyx/base/elementvector.h
+++ src/libaas/basyx/base/elementvector.h
@@ -1,18 +1,24 @@
 #ifndef ELEMENTVECTOR_H
 #define ELEMENTVECTOR_H
 
+#include <initializer_list>
 #include <vector>
 
 template<typename ElementType>
 class ElementVector: public std::vector<ElementType>
 {
 public:
-public:
    using element_t = ElementType;
-   ElementVector() {};
-   template<typename element_t>
-   void add(element_t element) { this->emplace_back(std::move(element));
+   ElementVector() = default;
+   ElementVector(std::initializer_list<element_t> list) {
+      auto iter = list.begin();
+      while(iter != list.end()) {
+         this->emplace_back(*iter);
+         iter++;
+      }
    }
+
+   void add(element_t element) { this->emplace_back(std::move(element)); }
 };
 
 #endif // ELEMENTVECTOR_H
