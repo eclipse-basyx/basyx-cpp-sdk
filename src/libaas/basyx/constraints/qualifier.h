5ec5771daa2456d3ef0d21c8a87d21d23f3b1ecc
Tobias Klausmann <tobias.klausmann@lenze.com>
2022-07-01 11:29:15 +0200

    Qualifier: Add a base class containing a type information for casting
    With this we can cast to and from the base
    
    Add test cases while we are at it!
    
    Signed-off-by: Tobias Klausmann <tobias.klausmann@lenze.com>

--- src/libaas/basyx/constraints/qualifier.h
+++ src/libaas/basyx/constraints/qualifier.h
@@ -1,3 +1,6 @@
+#ifndef QUALIFIER_H
+#define QUALIFIER_H
+
 #pragma once
 
 #include <basyx/base/valuetypedefs.h>
@@ -10,48 +13,72 @@
 
 #include <basyx/util/optional/optional.hpp>
 
+#include <basyx/serialization/serializable.h>
+
 namespace basyx
 {
 
 class qualifier_base : 
 	public Constraint,
-	public HasSemantics,
 	private ModelType<ModelTypes::Qualifier>
 {
 public:
 	qualifier_base() = default;
-	virtual ~qualifier_base() = 0;
+   virtual ~qualifier_base() = default;
 public:
 	virtual util::string_view getValueType() const = 0;
 };
 
+class QualifierBase: public qualifier_base {
+   basyx::detail::datatypes type;
+public:
+   QualifierBase(basyx::detail::datatypes type): type(type) {}
+   ~QualifierBase() = default;
+
+   util::string_view getValueType() const override { return basyx::detail::toString(type); }
+   basyx::detail::datatypes getValueTypeEnum() { return type; }
+
+   template <typename DataType>
+   Qualifier<DataType>* cast() { return dynamic_cast<Qualifier<DataType>*>(this); };
+};
+
 template<typename ValueType>
-class Qualifier : public qualifier_base
-{
+class Qualifier : public QualifierBase {
 public:
 	using modeltype_base::get_model_type;
 private:
-	std::string qualifierType;
+   std::string qualifierType;
 	util::optional<Reference> valueId;
 	util::optional<ValueType> value;
 public:
-	Qualifier(util::string_view qualifierType) : qualifierType(qualifierType.to_string()) {};
+   Qualifier(util::string_view qualifierType) :
+      QualifierBase(detail::data_type_def<ValueType>::value_type),
+      qualifierType(qualifierType.to_string()) {
+   }
 
 	template<typename U = ValueType>
-	Qualifier(util::string_view qualifierType, U && u) : qualifierType(qualifierType.to_string()), value(std::forward<U>(u)) {};
+   Qualifier(util::string_view qualifierType, U && u) :
+      QualifierBase(detail::data_type_def<ValueType>::value_type),
+      qualifierType(qualifierType.to_string()),
+      value(std::forward<U>(u)) {
+   }
 
-	Qualifier(const Qualifier&) = default;
+   Qualifier(const Qualifier&) = default;
 	Qualifier& operator=(const Qualifier&) = default;
 
 	Qualifier(Qualifier&&) = default;
 	Qualifier& operator=(Qualifier&&) = default;
 
-	~Qualifier() = default;
+   ~Qualifier() = default;
 public:
-	const std::string & getQualifierType() const { return qualifierType; };
-	void setQualifierType(util::string_view qualifierType) { this->qualifierType = qualifierType.to_string(); };
+   std::string & getQualifierType() { return qualifierType; };
+   void setQualifierType(util::string_view qualifierType) {
+      this->qualifierType = qualifierType.to_string();
+   };
 
-	util::string_view getValueType() const override { return detail::data_type_def<ValueType>::value_type; };
+   /*util::string_view getValueType() const override {
+      return basyx::detail::toString(detail::data_type_def<ValueType>::value_type);
+   }*/
 
 	const util::optional<ValueType> & getValue() const { return this->value; };
 
@@ -62,3 +89,5 @@ public:
 };
 
 }
+
+#endif // QUALIFIER_H
---
+++ tests/tests_libaas/test_qualifier.cpp
@@ -0,0 +1,153 @@
+#include <gtest/gtest.h>
+
+#include <basyx/key.h>
+#include <basyx/langstringset.h>
+#include <basyx/reference.h>
+#include <basyx/environment.h>
+#include <basyx/assetadministrationshell.h>
+#include <basyx/asset/asset.h>
+#include <basyx/asset/assetinformation.h>
+#include <basyx/submodel.h>
+
+#include <basyx/enums/IdentifiableElements.h>
+
+#include <basyx/constraints/qualifier.h>
+#include <basyx/constraints/formula.h>
+
+#include <basyx/base/basyx_enum_base.h>
+
+using namespace basyx;
+
+class QualifierTest : public ::testing::Test {
+protected:
+    // Test settings
+
+    // Test variables
+
+    virtual void SetUp()
+    {
+    }
+
+    virtual void TearDown()
+    {
+    }
+};
+
+
+TEST_F(QualifierTest, QualifierType)
+{
+    ASSERT_EQ(Qualifier<uint8_t> { "id" }.getValueType(), "unsignedByte");
+    ASSERT_EQ(Qualifier<int8_t> { "id" }.getValueType(), "byte");
+    ASSERT_EQ(Qualifier<std::string> { "id" }.getValueType(), "string");
+    ASSERT_EQ(Qualifier<double> { "id" }.getValueType(), "double");
+    ASSERT_EQ(Qualifier<float> { "id" }.getValueType(), "float");
+    ASSERT_EQ(Qualifier<char> { "id" }.getValueType(), "byte");
+};
+
+TEST_F(QualifierTest, QualifierTest1)
+{
+    Qualifier<int> q1{ "int_qual" };
+    q1.setValue(5);
+
+    ASSERT_EQ(*q1.getValue(), 5);
+    ASSERT_EQ(q1.getValueType(), "int");
+
+    Qualifier<float> p2{ "float_qualifier" };
+    ASSERT_EQ(p2.getValueType(), "float");
+    ASSERT_FALSE(p2.getValue());
+
+    p2.setValue(5.0f);
+    ASSERT_TRUE(p2.getValue());
+    ASSERT_EQ(*p2.getValue(), 5.0f);
+};
+
+TEST_F(QualifierTest, QualifierTest2)
+{
+    Qualifier<int> q1{ "int_qualifier", 5 };
+    ASSERT_EQ(*q1.getValue(), 5);
+    ASSERT_EQ(q1.getValueType(), "int");
+
+    Qualifier<float> q2{ "float_qualifier", 5.0f };
+    ASSERT_TRUE(q2.getValue());
+    ASSERT_EQ(*q2.getValue(), 5.0f);
+
+    Qualifier<std::string> q3{ "string_qualifier", "test" };
+    ASSERT_TRUE(q3.getValue());
+    ASSERT_EQ(*q3.getValue(), "test");
+};
+
+
+TEST_F(QualifierTest, IntQualifierCopy)
+{
+    Qualifier<int> i{ "i", 1 };
+    auto i2 = i;
+
+    i2.~Qualifier();
+    i.~Qualifier();
+}
+
+TEST_F(QualifierTest, StringQualifierCopy)
+{
+    Qualifier<std::string> s("stringType");
+    s.setValue("testStart");
+    auto s2 = s;
+
+    s.setValue("test");
+
+    s.~Qualifier();
+    s2.~Qualifier();
+}
+
+TEST_F(QualifierTest, QualifierTypeTest)
+{
+    std::string s = "string_qualifier";
+
+    Qualifier<std::string> qs{ "string_qualifier", "test" };
+    ASSERT_EQ(qs.getQualifierType(), s);
+
+    auto qs2 = qs;
+
+    ASSERT_EQ(qs2.getQualifierType(), s);
+}
+
+TEST_F(QualifierTest, StringQualifierInitializerListCopy)
+{
+    Qualifier<std::string> s{ "string_qualifier", "testStart" };
+    auto s2 = s;
+
+    s.setValue("test");
+}
+
+TEST_F(QualifierTest, QualifierCast)
+{
+    Qualifier<int> i{ "i", 1 };
+    QualifierBase* baseQual = &i;
+
+    auto casted_ok = baseQual->cast<int>();
+    ASSERT_NE(casted_ok, nullptr);
+    ASSERT_EQ(casted_ok->getValue(), i.getValue());
+
+    auto casted_bad = baseQual->cast<std::string>();
+    ASSERT_EQ(casted_bad, nullptr);
+}
+
+TEST_F(QualifierTest, QualifierTypeInfo)
+{
+    int intVal = 1;
+    Qualifier<int> i{ "i", intVal };
+    QualifierBase* baseQual = &i;
+
+    ASSERT_EQ(baseQual->getValueType(), "int");
+
+    switch (baseQual->getValueTypeEnum()) {
+    case basyx::detail::Integer: {
+       Qualifier<int> *i2= baseQual->cast<int>();
+       ASSERT_EQ(i2->getValue(), intVal);
+       break;
+    }
+    default:
+       break;
+    }
+}
+
+
